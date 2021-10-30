9155fbe0e3cefa503aee2aac6d1395bd1cef4d3f
Gabriel Bracamonte <gabobrako@gmail.com>
2021-10-30 18:23:19 -0400

    css hotfix

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const LEFT_ARROW = 37
+const UP_ARROW = 38
+const RIGHT_ARROW = 39
+const DOWN_ARROW = 40
+
+const ENTER = 13
+
+let cuadro1 = document.getElementById('cuadro1')
+let cuadro1 = document.getElementById('cuadro2')
+let cuadro1 = document.getElementById('cuadro3')
+
+let cuadro1 = document.getElementById('cuadro4')
+let cuadro1 = document.getElementById('cuadro5')
+let cuadro1 = document.getElementById('cuadro6')
+
+let cuadro1 = document.getElementById('cuadro7')
+let cuadro1 = document.getElementById('cuadro8')
+let cuadro1 = document.getElementById('cuadro9')
